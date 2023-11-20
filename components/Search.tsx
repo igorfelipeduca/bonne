81b5ad05823001350f44de7ca97dcb81e08594bb
Igor F. Duca <yelldutz@gmail.com>
2023-11-20 19:06:20 -0300

    fix: bonne logo search

--- components/Search.tsx
+++ components/Search.tsx
@@ -30,7 +30,7 @@ export default function CatalogSearch({ query, setQuery }: CatalogSearchProps) {
       </div>
 
       <div className="flex lg:hidden px-2 items-center gap-x-4 w-full">
-        <Image src={"/icon.svg"} alt="Icon" className="h-8 w-8" isBlurred />
+        <Image src={"/icon.svg"} alt="Icon" className="h-10 w-10" isBlurred />
 
         <div className="w-full px-2 lg:px-0 lg:w-2/3 border border-zinc-900 rounded-lg bg-black p-4 text-center flex justify-center">
           <div className="flex text-zinc-700 gap-x-2">
