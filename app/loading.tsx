54d9bd29fe7ecbbda8aca7ed802abf208419d012
Igor F. Duca <yelldutz@gmail.com>
2023-11-24 23:14:34 -0300

    fix: fixing loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,13 +2,8 @@ import { Image } from "@nextui-org/react";
 
 export default function Loading() {
   return (
-    <div className="w-screen h-screen bg-black items-center justify-center">
-      <Image
-        src={"/icon.svg"}
-        alt="Bonne"
-        className="h-32 w-32 animate-pulse"
-        isBlurred
-      />
+    <div className="w-screen h-screen flex bg-black items-center justify-center">
+      <Image src={"/icon.svg"} alt="Bonne" className="h-32 w-32" isBlurred />
     </div>
   );
 }
