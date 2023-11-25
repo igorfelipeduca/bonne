bd013dafafc5166b07868f574a924c319f6b6cc1
Igor F. Duca <yelldutz@gmail.com>
2023-11-24 23:27:46 -0300

    fix: home and cart responsivity

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,6 +1,7 @@
 import { Image } from "@nextui-org/react";
 import { Search } from "lucide-react";
 import CartSheet from "./CartSheet";
+import Link from "next/link";
 
 interface CatalogSearchProps {
   query: string;
@@ -11,7 +12,9 @@ export default function CatalogSearch({ query, setQuery }: CatalogSearchProps) {
   return (
     <>
       <div className="hidden lg:flex lg:justify-center items-center gap-x-4 w-full">
-        <Image src={"/icon.svg"} alt="Icon" className="h-10 w-10" isBlurred />
+        <Link href={"/"}>
+          <Image src={"/icon.svg"} alt="Icon" className="h-10 w-10" isBlurred />
+        </Link>
 
         <div className="w-full px-2 lg:px-0 lg:w-2/3 border border-zinc-900 rounded-lg bg-black p-4 text-center flex justify-center">
           <div className="flex text-zinc-700 gap-x-2">
@@ -30,7 +33,9 @@ export default function CatalogSearch({ query, setQuery }: CatalogSearchProps) {
       </div>
 
       <div className="flex lg:hidden px-2 items-center gap-x-4 w-full">
-        <Image src={"/icon.svg"} alt="Icon" className="h-10 w-10" isBlurred />
+        <Link href={"/"}>
+          <Image src={"/icon.svg"} alt="Icon" className="h-10 w-10" isBlurred />
+        </Link>
 
         <div className="w-full px-2 lg:px-0 lg:w-2/3 border border-zinc-900 rounded-lg bg-black p-4 text-center flex justify-center">
           <div className="flex text-zinc-700 gap-x-2">
